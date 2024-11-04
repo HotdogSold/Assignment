6d0bf4f4bef1248482ebb407bc2cafcbc6fad8fd
HotdogSold <162094227+HotdogSold@users.noreply.github.com>
2024-11-04 23:40:44 +0530

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+    Sub Main()
+        Dim person1 As New Person()
+        person1.DisplayInfo()
+
+        Dim person2 As New Person("Alice", 30)
+        person2.DisplayInfo()
+
+        ' Keep the console window open
+        Console.WriteLine("Press Enter to exit...")
+        Console.ReadLine()
+    End Sub
+
+    Public Class Person
+        Public Property Name As String
+        Public Property Age As Integer
+
+        Public Sub New()
+            Name = "Unknown"
+            Age = 0
+        End Sub
+
+        Public Sub New(ByVal name As String, ByVal age As Integer)
+            Me.Name = name
+            Me.Age = age
+        End Sub
+
+        Public Sub DisplayInfo()
+            Console.WriteLine($"Name: {Name}, Age: {Age}")
+        End Sub
+    End Class
+End Module
+
+
+
+
---
+++ Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConstructorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.DisplayInfo();
+
+            Person person2 = new Person("Alice", 30);
+            person2.DisplayInfo();
+
+            // Keep the console window open
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "Unknown";
+            Age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
