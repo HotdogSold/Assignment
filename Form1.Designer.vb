254ae118c058f42984952f3a9e8abd3c5789aabf
HotdogSold <162094227+HotdogSold@users.noreply.github.com>
2024-11-04 23:27:16 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtFirstNumber = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSecondNumber = New System.Windows.Forms.TextBox()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.btnExponentiate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(48, 298)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(190, 61)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtFirstNumber
+        '
+        Me.txtFirstNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFirstNumber.Location = New System.Drawing.Point(250, 21)
+        Me.txtFirstNumber.Name = "txtFirstNumber"
+        Me.txtFirstNumber.Size = New System.Drawing.Size(359, 45)
+        Me.txtFirstNumber.TabIndex = 6
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(12, 37)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(226, 29)
+        Me.label1.TabIndex = 9
+        Me.label1.Text = "Enter First Number"
+        '
+        'txtResult
+        '
+        Me.txtResult.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txtResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResult.Location = New System.Drawing.Point(189, 190)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ReadOnly = True
+        Me.txtResult.Size = New System.Drawing.Size(359, 45)
+        Me.txtResult.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 201)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 29)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Result"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 121)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(263, 29)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Enter Second Number"
+        '
+        'txtSecondNumber
+        '
+        Me.txtSecondNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.1!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSecondNumber.Location = New System.Drawing.Point(292, 110)
+        Me.txtSecondNumber.Name = "txtSecondNumber"
+        Me.txtSecondNumber.Size = New System.Drawing.Size(359, 45)
+        Me.txtSecondNumber.TabIndex = 21
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubtract.Location = New System.Drawing.Point(48, 377)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(190, 61)
+        Me.btnSubtract.TabIndex = 22
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiply.Location = New System.Drawing.Point(358, 298)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(190, 61)
+        Me.btnMultiply.TabIndex = 23
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivide.Location = New System.Drawing.Point(358, 377)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(190, 61)
+        Me.btnDivide.TabIndex = 24
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'btnExponentiate
+        '
+        Me.btnExponentiate.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExponentiate.Location = New System.Drawing.Point(701, 298)
+        Me.btnExponentiate.Name = "btnExponentiate"
+        Me.btnExponentiate.Size = New System.Drawing.Size(190, 61)
+        Me.btnExponentiate.TabIndex = 25
+        Me.btnExponentiate.Text = "^"
+        Me.btnExponentiate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.15!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(701, 377)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(190, 61)
+        Me.btnClear.TabIndex = 26
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1924, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExponentiate)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.txtSecondNumber)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.txtFirstNumber)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtFirstNumber As TextBox
+    Friend WithEvents label1 As Label
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSecondNumber As TextBox
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnExponentiate As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim firstNumber As Double = Double.Parse(txtFirstNumber.Text)
+        Dim secondNumber As Double = Double.Parse(txtSecondNumber.Text)
+        txtResult.Text = (firstNumber + secondNumber).ToString()
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        Dim firstNumber As Double = Double.Parse(txtFirstNumber.Text)
+        Dim secondNumber As Double = Double.Parse(txtSecondNumber.Text)
+        txtResult.Text = (firstNumber - secondNumber).ToString()
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        Dim firstNumber As Double = Double.Parse(txtFirstNumber.Text)
+        Dim secondNumber As Double = Double.Parse(txtSecondNumber.Text)
+        txtResult.Text = (firstNumber * secondNumber).ToString()
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        Dim firstNumber As Double = Double.Parse(txtFirstNumber.Text)
+        Dim secondNumber As Double = Double.Parse(txtSecondNumber.Text)
+        If secondNumber <> 0 Then
+            txtResult.Text = (firstNumber / secondNumber).ToString()
+        Else
+            MessageBox.Show("Cannot divide by zero")
+        End If
+    End Sub
+
+    Private Sub btnExponentiate_Click(sender As Object, e As EventArgs) Handles btnExponentiate.Click
+        Dim baseNumber As Double = Double.Parse(txtFirstNumber.Text)
+        Dim exponent As Double = Double.Parse(txtSecondNumber.Text)
+        txtResult.Text = (Math.Pow(baseNumber, exponent)).ToString()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtFirstNumber.Clear()
+        txtSecondNumber.Clear()
+        txtResult.Clear()
+    End Sub
+End Class
